3e43f17c2acfa33a6db28c483ca191823ee933e2
Rohit Yadav <rohitkyadav2312@gmail.com>
2025-09-16 10:15:04 +0530

    Update  _app.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Document, { DocumentContext,Head,Html,Main,NextScript } from "next/document";
 
-const _document = () => {
-  return (
-    <div>_document</div>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps ;
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="stylesheet" href="/favicon.icon" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default _document
+export default MyDocument;
